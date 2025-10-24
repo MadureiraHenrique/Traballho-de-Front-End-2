3222b699e7521062d32c499d795f1176f2749332
Henrique Madureira <carloshenriquemadureira.santos@ucsal.edu.br>
2025-10-24 01:01:42 -0300

    Esqueleto do projeto criado, assim como estilização e semântica apropriada para a Landing Page

---
+++ JS/App.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", (evento) => {
+    // Implementar Toogle Dark(Purple)/White(Purple).
+    // "" Componente para visualizar séries, videos, animes, etc, (...).
+    // "" Validação Básica para caso venha alguém favoritar algo do catálogo.
+});
